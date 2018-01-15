6ff2de2cde45490936a3249b7bfa059dcc277d88
Patrick Hurley <phurley@gmail.com>
2018-01-15 18:04:44 -0500

    Add cheesy path following to our code

--- src/org/usfirst/frc862/glitch/Constants.java
+++ src/org/usfirst/frc862/glitch/Constants.java
@@ -11,31 +11,91 @@ public class Constants extends ConstantsBase {
     // it there, the value will be ignored
     // as the value here in the code is 
     // immutable
-        
-	//Logging
+
+    //Logging
     public static int logDepth = 1000;
 
     public static double WheelCircumference = LightningMath.in2ft(4) * Math.PI;
     public static double dead_band = 0.05;
-  public static int TALON_TIMEOUT = 10; //ms
+    public static int TALON_TIMEOUT = 10; //ms
 
-  //PID
-  public static double PHYSICAL_MAX_SPEED_TICKS = 850;
-  public static double MAX_SPEED_TICKS = 600;
+    //PID
+    public static double PHYSICAL_MAX_SPEED_TICKS = 850;
+    public static double MAX_SPEED_TICKS = 600;
 
-  // NOTE we are going to want to tune for high/low gear with
-  // at the very least a different feed forward...
-  public static double DRIVE_P = 0;
-  public static double DRIVE_I = 0;
-  public static double DRIVE_D = 0;
-  public static double DRIVE_F = 1024.0 / PHYSICAL_MAX_SPEED_TICKS;
+    // NOTE we are going to want to tune for high/low gear with
+    // at the very least a different feed forward...
+    public static double DRIVE_P = 0;
+    public static double DRIVE_I = 0;
+    public static double DRIVE_D = 0;
+    public static double DRIVE_F = 1024.0 / PHYSICAL_MAX_SPEED_TICKS;
 
-  public static double TICS_PER_ROTATION = 360;  // might be wrong!!!
+    public static double TICS_PER_ROTATION = 360;  // might be wrong!!!
 
-    // Cheesy Constants
+    // PID gains for drive velocity loop (HIGH GEAR)
+    // Units: setpoint, error, and output are in inches per second.
+    public static double kDriveHighGearVelocityKp = 1.2;
+    public static double kDriveHighGearVelocityKi = 0.0;
+    public static double kDriveHighGearVelocityKd = 6.0;
+    public static double kDriveHighGearVelocityKf = .15;
+    public static int kDriveHighGearVelocityIZone = 0;
+    public static double kDriveHighGearVelocityRampRate = 240.0;
+    public static double kDriveHighGearNominalOutput = 0.5;
+    public static double kDriveHighGearMaxSetpoint = 17.0 * 12.0; // 17 fps
+
+    /* ROBOT PHYSICAL CONSTANTS */
+
+    // Wheels
+    public static double kDriveWheelDiameterInches = 3.419;
+    public static double kTrackWidthInches = 26.655;
+    public static double kTrackScrubFactor = 0.924;
+
+    // Geometry
+    public static double kCenterToFrontBumperDistance = 16.33;
+    public static double kCenterToIntakeDistance = 23.11;
+    public static double kCenterToRearBumperDistance = 16.99;
+    public static double kCenterToSideBumperDistance = 17.225;
+
+    // Pose of the camera frame w.r.t. the robot frame
+    public static double kCameraXOffset = -3.3211;
+    public static double kCameraYOffset = 0.0;
+    public static double kCameraZOffset = 20.9;
+    public static double kCameraPitchAngleDegrees = 29.56; // Measured on 4/26
+    public static double kCameraYawAngleDegrees = 0.0;
+    public static double kCameraDeadband = 0.0;
+
+    // Path following constants
+    public static double kMinLookAhead = 12.0; // inches
+    public static double kMinLookAheadSpeed = 9.0; // inches per second
+    public static double kMaxLookAhead = 24.0; // inches
+    public static double kMaxLookAheadSpeed = 120.0; // inches per second
+    public static double kDeltaLookAhead = kMaxLookAhead - kMinLookAhead;
+    public static double kDeltaLookAheadSpeed = kMaxLookAheadSpeed - kMinLookAheadSpeed;
+
+    public static double kInertiaSteeringGain = 0.0; // angular velocity command is multiplied by this gain *
+    // our speed
+    // in inches per sec
     public static double kSegmentCompletionTolerance = 0.1; // inches
     public static double kPathFollowingMaxAccel = 120.0; // inches per second^2
-    
+    public static double kPathFollowingMaxVel = 120.0; // inches per second
+    public static double kPathFollowingProfileKp = 5.00;
+    public static double kPathFollowingProfileKi = 0.03;
+    public static double kPathFollowingProfileKv = 0.02;
+    public static double kPathFollowingProfileKffv = 1.0;
+    public static double kPathFollowingProfileKffa = 0.05;
+    public static double kPathFollowingGoalPosTolerance = 0.75;
+    public static double kPathFollowingGoalVelTolerance = 12.0;
+    public static double kPathStopSteeringDistance = 9.0;
+
+
+    // Goal tracker constants
+    public static double kMaxGoalTrackAge = 1.0;
+    public static double kMaxTrackerDistance = 18.0;
+    public static double kCameraFrameRate = 30.0;
+    public static double kTrackReportComparatorStablityWeight = 1.0;
+    public static double kTrackReportComparatorAgeWeight = 1.0;
+
+
     public String getFileName() {
         return "~/glitch.yaml";
     }
@@ -45,7 +105,7 @@ public class Constants extends ConstantsBase {
 //        Constants.lookupTable.put(1.0, 1.0);
 //        Constants.lookupTable.put(50.0, 2500.0);
 //        Constants.lookupTable.put(100.0, 10000.0);
-        
+
         File file = new File("/home/lvuser/echo");
         if (file.exists()) {
             // echo specific over-rides can go here
