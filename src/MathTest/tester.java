fd9d3a6b9fcb3bc5cc66599abae766ece32d6fa4
TannerAvery1022 <charmda3rd@outlook.com>
2018-01-31 21:27:07 -0500

    End of Wednesday
    We wrote more things and played with Gradle a little bit

--- src/MathTest/tester.java
+++ src/MathTest/tester.java
@@ -3,7 +3,7 @@ package MathTest;
 public class tester {
 
 	// change all of these values I just put the ones that were in the video
-	//
+	// The stall torque, or max torque
 	private static double kStallTorque = 2.402;
 	// Stall current in amps
 	private static double kStallCurrent = 126.145;
@@ -25,28 +25,33 @@ public class tester {
 	private static double kG = 72.0 / 12.0 * 22.0 / 16.0;
 	// Radius of pulley
 	private static double kr = 0.25 * 0.0254 * 22.0 / Math.PI / 2.0;
-	//control loop time step
+	// control loop time step
 	private static double kDt = 0.010;
 	
-	private double velocity, position;
+	private static double velocity = 0.0;
+	private static double position = 0.0;
 	
 	// V = I * R + omega / Kv
 	// torque = Kt * I
 	
 	public static void main(String[] args) {
 		
-		
+		simulateTime(1, 1);
 	}
 	
-	private double getAcceleration(double voltage) {
+	private static double getAcceleration(double voltage) {
 		return -Kt * kG / (Kv * kResistance * Math.pow(kr, 2) * kMass) * velocity + kG * Kt / (kResistance * kr * kMass) * voltage;
 	}
 	
-	private void simulateTime(double voltage, double time) {
-		
+	private static void simulateTime(double voltage, double time) {
+		double kSimTime = 0.0001;
 		double currentTime = 0;
-		while () {
-			
+		while (currentTime < time) {
+			double acc = getAcceleration(voltage);
+			position += velocity * currentTime;
+			velocity += acc * currentTime;
+			currentTime += kSimTime;
+			System.out.println(position);
 		}
 		
 	}
---
+++ src/MathTest/testerLoop.java
@@ -0,0 +1,10 @@
+package MathTest;
+
+public class testerLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
